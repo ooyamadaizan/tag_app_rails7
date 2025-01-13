fb13499d7d3906a0f9b1eb9cf77da7d7ba5870dc
ooyamadaizan <daizandeisia@gmail.com>
2025-01-13 23:42:47 +0900

    新規と編集ページでのみ読み込まれる設定に変更

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -1,1 +1,6 @@
-console.log("読み込み完了");
+document.addEventListener("turbo:load", () => {
+  const tagNameInput = document.querySelector("#post_form_tag_name");
+    if (tagNameInput){
+      console.log("読み込み完了");
+    };
+  });
